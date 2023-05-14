bde0ca9723e4fdc59f5e912fc06c4933210fa0f8
owell andry <81540116+owellandry@users.noreply.github.com>
2023-05-14 17:12:46 +0000

    Updates files: 12:12

--- script.js
+++ script.js
@@ -96,38 +96,24 @@ function pintar(p) {
       }
       function dropFinal(e) {
         e.preventDefault();
-        var puntero = e.target;
-        var padre = document.getElementById(puntero.id).childNodes;   
-        var item = e.dataTransfer.getData("Text");
-        var puedoPoner = cortaCompa(padre, item);
-        
-        if (puntero.id != "torre_1" && puntero.id != "torre_2" && puntero.id != "torre_3" && item != '' && puedoPoner) {
-          // Si la torre de destino tiene bloques
-          if (padre.length > 0) {
-            // Obtenemos el tamaño del bloque que se va a mover y el de la cima de la torre de destino
-            var tamanoBloque = parseInt(item.split('_')[1]);
-            var tamanoBloqueDestino = parseInt(padre[0].id.split('_')[1]);
+        const destino = e.target;
+        if(!destino.id.startsWith("torre_")) return;
       
-            // Si el bloque que se va a mover es más grande que el que se encuentra en la cima de la torre de destino, reiniciamos la partida
-            if (tamanoBloque > tamanoBloqueDestino) {
-              alert('Has movido un bloque grande encima de uno pequeño. La partida se reiniciará');
-              location.reload();
-              return;
-            }
-          }
+        const piezas = Array.from(destino.children).map(child => parseInt(child.id.split("_")[1]));
+        const item = e.dataTransfer.getData("Text");
+        const numItem = parseInt(item.split("_")[1]);
+        if(isNaN(numItem)) return;
       
-          // Movemos el bloque normalmente si todo está correcto
-          var quitar = document.getElementById(item);
-          quitar.parentNode.removeChild(quitar);
-          puntero.innerHTML = '<div id="'+item+'" draggable="true"></div>' + puntero.innerHTML;
+        if(piezas.length === 0 || numItem < piezas[0]) {
+          const itemElement = document.getElementById(item);
+          itemElement.parentNode.removeChild(itemElement);
+          destino.prepend(itemElement);
           contador++;
         }
-        
-        var sal = document.querySelector("div#salida span");
+      
+        const sal = document.querySelector("#salida span");
         sal.innerHTML = contador;
-        inicio();
       }
-        
       function pintar(p) {
         const cajas = document.getElementById(p);
         cajas.innerHTML = '';
@@ -151,3 +137,43 @@ function cortaCompa(primero, segundo) {
     return salida;
 }
 window.addEventListener("load", main, false);
+
+
+
+
+
+// Variables globales
+let cronometro = document.getElementById('cronometro');
+let startBtn = document.getElementById('start-btn');
+let segundos = 0;
+let minutos = 0;
+let horas = 0;
+let interval;
+
+// Función para actualizar el cronómetro
+function actualizarCronometro() {
+  segundos++;
+
+  if (segundos >= 60) {
+    segundos = 0;
+    minutos++;
+
+    if (minutos >= 60) {
+      minutos = 0;
+      horas++;
+    }
+  }
+
+  cronometro.textContent = (horas ? (horas > 9 ? horas : "0" + horas) : "00") + ":" + 
+                            (minutos ? (minutos > 9 ? minutos : "0" + minutos) : "00") + ":" + 
+                            (segundos > 9 ? segundos : "0" + segundos);
+}
+
+// Función para iniciar el cronómetro
+function iniciarCronometro() {
+  interval = setInterval(actualizarCronometro, 1000);
+  startBtn.disabled = true;
+}
+
+// Asignar el evento 'click' al botón para iniciar el cronómetro
+startBtn.addEventListener('click', iniciarCronometro);
